c9c8701669689a5435c60da1f552dad27452a9ba
macpat83 <macpat@gmail.com>
2022-04-08 21:22:28 -0500

    html update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,91 @@
+let questions = [{
+    number: 1,
+    question:"",
+    selections: [
+        // a
+        // b
+        // c
+        // d
+    ],
+    answer: ""
+},
+
+{
+number: 2,
+    question:"",
+    selections: [
+        // a
+        // b
+        // c
+        // d
+
+    ],
+    answer:"",
+},
+
+{
+    number: 3,
+        question:"",
+        selections: [
+            // a
+            // b
+            // c
+            // d
+
+        ],
+        answer:"",
+    },
+
+    {
+        number: 4,
+            question:"",
+            selections: [
+                // a
+                // b
+                // c
+                // d
+    
+            ],
+            answer:"",
+        },
+
+        {
+number: 5,
+    question:"",
+    selections: [
+        // a
+        // b
+        // c
+        // d
+
+    ],
+    answer:"",
+}
+
+];
+
+
+
 var timerEl = document.getElementById('countdown');
 var start_bttn = document.querySelector(".start-btn button");
+var quit_bttn = document.querySelector(".buttons button")
+var restart_bttn = document.querySelector(".buttons button")
 var container = document.querySelector("container");
 const question_text = document.querySelector(".question_text");
 const selection_text = document.querySelector(".selection_text");
+var quiz = document.querySelector(".quiz")
+var results = document.querySelector(".results")
+
+//start quiz
+start_bttn.onclick = ()=> {
+    quiz.classList.add("showQuiz");
+
+}
+
+quit_bttn.onclick = ()=> {
+    quiz.classList.remove("showQuiz");
+}
+
 
 function countdown() {
     var timeLeft = 60;
@@ -22,74 +105,13 @@ function countdown() {
     
     }
 
-    let questions = [{
-        number: 1,
-        question:"",
-        selections: [
-            // a
-            // b
-            // c
-            // d
-        ],
-        answer: ""
-    },
 
-    {
-    number: 2,
-        question:"",
-        selections: [
-            // a
-            // b
-            // c
-            // d
-
-        ],
-        answer:"",
-    },
-
-    {
-        number: 3,
-            question:"",
-            selections: [
-                // a
-                // b
-                // c
-                // d
-    
-            ],
-            answer:"",
-        },
-
-        {
-            number: 4,
-                question:"",
-                selections: [
-                    // a
-                    // b
-                    // c
-                    // d
-        
-                ],
-                answer:"",
-            },
-
-            {
-    number: 5,
-        question:"",
-        selections: [
-            // a
-            // b
-            // c
-            // d
-
-        ],
-        answer:"",
-    }
-];
 
 // function to run quiz
 function runQuiz(){
 }
+
+
 
     
 
